e3d7d9ac953be58d7db42f955cf14ab48a37e478
Komche <Adamoukomcheabdoulrazak@gmail.com>
2020-07-25 20:57:03 +0100

    theme pour sonf

--- req.sql
+++ req.sql
@@ -1,7 +1,7 @@
 //Envigueur
 ALTER TABLE `agence` ADD `file` INT NULL AFTER `ville`, ADD INDEX (`file`);
 
-//25 Juiilet 2020
+-- //25 Juiilet 2020
 ALTER TABLE `tarif` ADD `vdepart` INT NULL AFTER `valeur`, ADD `vdestination` INT NULL AFTER `vdepart`, ADD INDEX (`vdestination`), ADD INDEX (`vdepart`);
 ALTER TABLE `ville` DROP `tarif`;
 
