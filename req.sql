4782f99c982b12c554c9cef381900dde46cdd2b5
227Mahaman <zalkiabass.456@gmail.com>
2020-07-25 16:35:59 +0100

    Requetes pour l'ajustement de la reservation

--- req.sql
+++ req.sql
@@ -4,3 +4,6 @@ ALTER TABLE `agence` ADD `file` INT NULL AFTER `ville`, ADD INDEX (`file`);
 //25 Juiilet 2020
 ALTER TABLE `tarif` ADD `vdepart` INT NULL AFTER `valeur`, ADD `vdestination` INT NULL AFTER `vdepart`, ADD INDEX (`vdestination`), ADD INDEX (`vdepart`);
 ALTER TABLE `ville` DROP `tarif`;
+
+ALTER TABLE `billet` DROP `depart_destination`;
+ALTER TABLE `billet` ADD `depart` INT NULL AFTER `bus`, ADD `destination` INT NULL AFTER `depart`, ADD INDEX (`depart`), ADD INDEX (`destination`);
